d0a2158fb889eff3d02fc3d9e98cb1f3297f45ea
Abdul Ghaffar <aghaffar570@gmail.com>
2018-07-03 17:18:46 -0400

    update

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const { exec } = require('child_process');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const PORT = process.env.PORT || 8081;
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: 'http://localhost:8080'
+  })
+);
 
 app.get('/', (req, res, nex) => {
   res.json('inside docker');
@@ -16,8 +22,8 @@ app.post('/secApp', (req, res, next) => {
   exec(`node test "${req.body.code}"`, (error, stdout, stderr) => {
     if (error) console.log(error);
     console.log('RESULT FROM DOCKER: ', stdout);
+    res.json(stdout);
   });
-  res.json('leave now!');
 });
 
 app.listen(PORT, () => {
